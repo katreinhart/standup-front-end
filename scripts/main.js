36d41ebc3c642382bbe39112decf97624662069c
Kat Reinhart <kat.reinhart@gmail.com>
2017-10-23 10:30:14 -0700

    Static front-end draft for stand-up website

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+console.log('sanity check!');
+
+const timerButton = document.getElementById('timer-button')
+timerButton.addEventListener('click', (e) => {
+  console.log('Start timer clicked!')
+})
